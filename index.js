315ae6755eb5dfc09b0bcece05b7ef4bbfd84951
byd14 <wrenchtop@gmail.com>
2023-05-10 14:26:38 +0800

    server side cells, cell ownership and rent

--- index.js
+++ index.js
@@ -1,57 +1,37 @@
-class grid_cell {
-    constructor(coords){
-        this.coords = coords;
-        this.start_price = 100;
-        this.price_step = 10;
-        this.price_ceiling = 200;
-        this.pliability = 0.5;
-    };
-}
-
-var grid_cells = {
-    0: new grid_cell({x: 0,y: 0}),
-    1: new grid_cell({x:1 ,y:0 }),
-    2: new grid_cell({x:2 ,y:0 }),
-    3: new grid_cell({x:3 ,y:0 }),
-    4: new grid_cell({x:4 ,y:0 }),
-    5: new grid_cell({x:4 ,y:1 }),
-    6: new grid_cell({x:4 ,y:2 }),
-    7: new grid_cell({x:4 ,y:3 }),
-    8: new grid_cell({x:4 ,y:4 }),
-    9: new grid_cell({x: 3,y: 4}),
-    10: new grid_cell({x: 2,y: 4}),
-    11: new grid_cell({x: 1,y: 4}),
-    12: new grid_cell({x: 0,y: 4}),
-    13: new grid_cell({x: 0,y: 3}),
-    14: new grid_cell({x: 0,y: 2}),
-    15: new grid_cell({x: 0,y: 1})
-};
-
 var cell_size = 120;
 
 function cell_action(cell_num) {
-    switch (cell_num) {
-        default:
-            console.log('hey)');
-            const trade_element = document.createElement('div');
-            trade_element.classList.add('trade_popup');
-            trade_element.id = 'closed_trade';
-            trade_element.innerHTML = ('\
-            <button onclick="tradeAction(true)">bet</button>\
-            <button onclick="tradeAction(false)">leave</button>\
-            <p>current cost</p>\
-            <p id = cost>0</p>\
-            ');
-            play_space.appendChild(trade_element);
-            document.getElementById('cost').innerText = grid_cells[players[playerId].cell].start_price;
-            break;
-    }   
+    if (cells[cell_num].owner == 'npc') {
+        switch (cell_num) {
+            default:
+                console.log('hey)');
+                const trade_element = document.createElement('div');
+                trade_element.classList.add('trade_popup');
+                trade_element.id = 'closed_trade';
+                trade_element.innerHTML = ('\
+                <button onclick="tradeAction(true)">bet</button>\
+                <button onclick="tradeAction(false)">leave</button>\
+                <p>current cost</p>\
+                <p id = cost>0</p>\
+                ');
+                play_space.appendChild(trade_element);
+                document.getElementById('cost').innerText = cells[players[playerId].cell].price;
+                break;
+        }
+    } else {
+        if (cells[cell_num].owner != playerId) {
+            console.log('rent');
+            const updates = {};
+            updates['/players/' + playerId + '/balance/'] = players[playerId].balance - cells[cell_num].rent;
+            update(ref(database), updates);
+        }
+    }
 }
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-app.js";
 import { getAuth, signInAnonymously, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-auth.js"
-import { getDatabase, ref, set, onValue, onDisconnect, onChildAdded, onChildRemoved } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-database.js"
+import { getDatabase, ref, set, onValue, onDisconnect, onChildAdded, onChildRemoved, update } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-database.js"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -73,14 +53,22 @@ const auth = getAuth(app);
 
 let playerId;
 let playerRef;
-let playerBalance = 1000;
 let players = {};
 let playerElements = {};
+
+let cellsRef;
+let cells;
 
 const play_space = document.getElementById('play-space');
 
 function game_init() {
     const all_players_ref = ref(database, '/players')
+
+    onValue(cellsRef, (snapshot) => {
+
+        cells = snapshot.val();
+
+    });
     
     onValue(all_players_ref, (snapshot) => {
 
@@ -88,8 +76,8 @@ function game_init() {
         Object.keys(players).forEach((key) => {
             const characterState = players[key];
             let el = playerElements[key];
-            el.style.left = 160 + grid_cells[players[key].cell].coords.x * cell_size;
-            el.style.top = 100 + grid_cells[players[key].cell].coords.y * cell_size;
+            el.style.left = 160 + cells[players[key].cell].coords.x * cell_size;
+            el.style.top = 100 + cells[players[key].cell].coords.y * cell_size;
 
         });
 
@@ -133,9 +121,11 @@ onAuthStateChanged(auth, (user) => {
         playerRef = ref(database, '/players/' + playerId);
         set(playerRef, {
             id: playerId,
-            balance: playerBalance,
+            balance: 1000,
             cell: 0
         });
+
+        cellsRef = ref(database, '/cells/');
 
         //Initialize game for connected player
         game_init();
@@ -153,25 +143,30 @@ window.diceRoll = function diceRoll(){
     var roll = Math.floor(Math.random() * 6 + 1)
     cell_action((roll + players[playerId].cell) % 16);
     console.log(roll);
-    set(playerRef, {
-        id: playerId,
-        cell: (roll + players[playerId].cell) % 16,
-        balance: playerBalance
-    });
+    const updates = {};
+    updates['/players/' + playerId + '/cell/'] = (roll + players[playerId].cell) % 16;
+    update(ref(database), updates);
 }
 
 window.tradeAction = function tradeAction(code) {
     if (code) {
         const cost_value = document.getElementById('cost');
-        const current_cell = grid_cells[players[playerId].cell]
-        if (Number(cost_value.innerText) < current_cell.price_ceiling) {        
+        const current_cell = cells[players[playerId].cell]
+        if (Number(cost_value.innerText) < current_cell.ceiling) {        
             if (Math.random() > current_cell.pliability) {
-                    cost_value.innerText = Number(cost_value.innerText) + current_cell.price_step;
-                } else {
-                    play_space.removeChild(document.getElementById('closed_trade'));
+                cost_value.innerText = Number(cost_value.innerText) + current_cell.step;
+            } else {
+                play_space.removeChild(document.getElementById('closed_trade'));
+                const updates = {};
+                updates['/cells/' + players[playerId].cell + '/owner/'] = playerId;
+                update(ref(database), updates);
                 }
         } else {
             play_space.removeChild(document.getElementById('closed_trade'));
+            const updates = {};
+            updates['/cells/' + players[playerId].cell + '/owner/'] = playerId;
+            update(ref(database), updates);
+
         }
     } else {
         play_space.removeChild(document.getElementById('closed_trade'));
